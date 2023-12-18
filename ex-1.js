2913274036b8de6a497972f68037fe7b1b615b44
Pao <chaiwatt77@gmail.com>
2023-12-18 13:47:17 +0700

    ex1-2

--- ex-1.js
+++ ex-1.js
@@ -2,6 +2,8 @@
 
 const addStrings = function (num1, num2) {
   //Start coding here
+  let sum = Number(num1)+Number(num2);
+  return String(sum);
 };
 
 const result1 = addStrings("11", "123");
@@ -11,3 +13,19 @@ const result3 = addStrings("0", "0");
 console.log(result1); // "134"
 console.log(result2); // "533"
 console.log(result3); // "0"
+
+/*
+const addStrings = function (num1, num2) {
+  //Start coding here
+  let sum=+num1+(+num2)
+  return ""+sum;
+};
+
+const result1 = addStrings("11", "123");
+const result2 = addStrings("456", "77");
+const result3 = addStrings("0", "0");
+
+console.log(result1); // "134"
+console.log(result2); // "533"
+console.log(result3); // "0"
+*/
--- ex-2.js
+++ ex-2.js
@@ -1,13 +1,15 @@
 //Exercise 2: Valid Palindrome
-
 const isPalindrome = function (s) {
   //Start Coding here
+  s=s.toLowerCase().replaceAll(/\W/g,"");
+  let reverse = s.split("").reverse().join("");
+  return s === reverse;
 };
-
+  
 const result1 = isPalindrome("A man, a plan, a canal: Panama");
 const result2 = isPalindrome("race a car");
 const result3 = isPalindrome(" ");
-
+  
 console.log(result1); // true
 console.log(result2); // false
 console.log(result3); // true
